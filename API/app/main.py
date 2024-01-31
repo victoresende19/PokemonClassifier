6404aa6b425d5e526b723ed14ddcd5b04c97fa45
Victor Resende <victoresende19@gmail.com>
2024-01-31 09:50:22 -0300

    :star: middlewares changes allowed methods :star:

--- API/app/main.py
+++ API/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_origins=["*"],
     allow_credentials=True,
     allow_headers=["*"],
